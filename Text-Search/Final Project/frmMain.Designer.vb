bbdcecc7e3846faba06ca74db0b8502025154613
elizabethxlee <39136626+elizabethxlee@users.noreply.github.com>
2019-02-01 21:01:19 -0800

    Add Text-Search Project files

---
+++ Text-Search/Final Project/frmMain.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.btnCount = New System.Windows.Forms.Button()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtDisplay = New System.Windows.Forms.RichTextBox()
+        Me.lblDirections = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(34, 109)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(188, 20)
+        Me.txtSearch.TabIndex = 0
+        '
+        'btnCount
+        '
+        Me.btnCount.Location = New System.Drawing.Point(228, 109)
+        Me.btnCount.Name = "btnCount"
+        Me.btnCount.Size = New System.Drawing.Size(94, 23)
+        Me.btnCount.TabIndex = 1
+        Me.btnCount.Text = "C&ount"
+        Me.btnCount.UseVisualStyleBackColor = True
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(333, 109)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(94, 23)
+        Me.btnDisplay.TabIndex = 2
+        Me.btnDisplay.Text = "&Display Verses"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(433, 109)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(94, 23)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(472, 270)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtDisplay
+        '
+        Me.txtDisplay.Location = New System.Drawing.Point(34, 138)
+        Me.txtDisplay.Name = "txtDisplay"
+        Me.txtDisplay.ReadOnly = True
+        Me.txtDisplay.Size = New System.Drawing.Size(493, 96)
+        Me.txtDisplay.TabIndex = 5
+        Me.txtDisplay.Text = ""
+        '
+        'lblDirections
+        '
+        Me.lblDirections.AutoSize = True
+        Me.lblDirections.BackColor = System.Drawing.Color.Transparent
+        Me.lblDirections.Location = New System.Drawing.Point(31, 93)
+        Me.lblDirections.Name = "lblDirections"
+        Me.lblDirections.Size = New System.Drawing.Size(117, 13)
+        Me.lblDirections.TabIndex = 6
+        Me.lblDirections.Text = "Enter a word or phrase:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.BackgroundImage = Global.Final_Project.My.Resources.Resources.bible_search
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(34, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 70)
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.BackColor = System.Drawing.Color.Transparent
+        Me.lblTitle.Font = New System.Drawing.Font("Bernard MT Condensed", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(140, 29)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(168, 38)
+        Me.lblTitle.TabIndex = 8
+        Me.lblTitle.Text = "Bible Search"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Final_Project.My.Resources.Resources.light_1282314_960_720
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(559, 305)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.lblDirections)
+        Me.Controls.Add(Me.txtDisplay)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.btnCount)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Bible Search"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents btnCount As Button
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtDisplay As RichTextBox
+    Friend WithEvents lblDirections As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblTitle As Label
+End Class
---
+++ Text-Search/Final Project/frmMain.vb
@@ -0,0 +1,195 @@
+﻿'Name: ReadFile Project 3
+'Purpose: Find the number of times a word is found in the entire file
+'Programmer: Elizabeth Lee
+Public Class frmMain
+    'Declare variables
+    Const DATA_FILE_NAME As String = "Datafile.txt"
+    Dim inFile As IO.StreamReader
+
+
+
+    '
+    '
+    '*******************SUBROUTINES********************
+    '
+    '
+
+    'Purpose: Checks if the file exists
+    Private Sub Check_If_File_Exists(ByRef inFile As IO.StreamReader)
+        If IO.File.Exists(DATA_FILE_NAME) = True Then
+            'if the file exists, opens the file
+            inFile = IO.File.OpenText(DATA_FILE_NAME)
+        Else
+            'If the file doesn't exist, an error message is displayed
+            MessageBox.Show("The File '" + DATA_FILE_NAME + "'" + " Does Not Exist", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Me.Close()
+        End If
+    End Sub
+
+
+    'Purpose: displays all the lines that contain the search word into the textbox
+    Private Sub findLinesWithSearchWord(ByRef inFile As IO.StreamReader, ByVal strLineFromFile As String, ByVal intIndex As Integer, ByVal strSearchWord As String)
+        Do Until inFile.Peek = -1
+            strLineFromFile = inFile.ReadLine + ControlChars.NewLine + ControlChars.NewLine
+            intIndex = strLineFromFile.ToUpper.IndexOf(strSearchWord.ToUpper)
+            If intIndex <> -1 Then
+                txtDisplay.Text = txtDisplay.Text + strLineFromFile
+            End If
+        Loop
+        If txtDisplay.Text = String.Empty Then
+            txtDisplay.Text = "word/phrase does not appear in the text file"
+        End If
+    End Sub
+
+
+    'Purpose: bolds and highlights the user's search word in all the displayed verses
+    Private Sub boldAndHighlight(ByVal strSearchWord As String)
+        txtDisplay.SuspendLayout()
+        Dim PreviousPosition As Integer = txtDisplay.SelectionStart
+        Dim PreviousSelection As Integer = txtDisplay.SelectionLength
+        Dim SelectionIndex As Integer = -1
+
+        Using BoldFont As New Font(txtDisplay.Font, FontStyle.Bold)
+            While True
+                SelectionIndex = txtDisplay.Find(strSearchWord, SelectionIndex + 1, RichTextBoxFinds.None)
+                'Stops search if there is no more text
+                If SelectionIndex < 0 Then Exit While
+                'Indicates what portion of the text should be bolded & highlighted
+                txtDisplay.SelectionStart = SelectionIndex
+                txtDisplay.SelectionLength = strSearchWord.Length
+                'Chooses the font style of the user's serach word
+                txtDisplay.SelectionFont = BoldFont
+                txtDisplay.SelectionColor = Color.Blue
+            End While
+        End Using
+        txtDisplay.ResumeLayout()
+        txtDisplay.SelectionStart = PreviousPosition
+        txtDisplay.SelectionLength = PreviousSelection
+    End Sub
+
+
+    'Purpose: Counts the number of times the user's search word appears in the entire file
+    Private Function CountOccurrences(ByRef inFile As IO.StreamReader, ByRef strEntireFile As String, ByVal strSearchWord As String, ByRef intIndex As Integer, ByRef intCounter As Integer) As Integer
+        'If the word does not appear at all, Do While loop will not process
+        intIndex = strEntireFile.ToUpper.IndexOf(strSearchWord.ToUpper)
+
+        Do While intIndex <> -1
+            intCounter += 1
+            strEntireFile = strEntireFile.Substring(intIndex + 1)
+            intIndex = strEntireFile.ToUpper.IndexOf(strSearchWord.ToUpper)
+        Loop
+        'Returns the number of times the word  appears in the entire file
+        Return intCounter
+    End Function
+
+
+    '
+    '
+    '*****************EVENT PROCEDURES******************************8
+    '
+    '
+
+
+    'EVENT: The program is run
+    'Purpose: Calls the subroutine 'Check_If_File_Exists' as soon as program is run
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Calls subroutine to check if the file exists
+        Call Check_If_File_Exists(inFile)
+    End Sub
+
+
+    'EVENT:     user presses 'Count' button
+    'Purpose:   Counts the number of times a word is found in the text file
+    Private Sub btnCount_Click(sender As Object, e As EventArgs) Handles btnCount.Click
+        Dim strSearch As String = txtSearch.Text
+        Dim intCounter As Integer
+        Dim strEntireFile As String = ""
+        Dim intIndex As Integer
+        txtDisplay.Text = ""
+
+        'prevents null reference exception
+        Call Check_If_File_Exists(inFile)
+
+        'Checks if user input is valid or if it is an empty string
+        Select Case strSearch
+            Case Is = String.Empty
+                txtDisplay.Text = "Invalid entry"
+            Case Else
+                'stores all the text from the file into a string variable
+                Do Until inFile.Peek = -1
+                    strEntireFile = inFile.ReadLine + ControlChars.NewLine
+                    'Invokes CountOccurrences function
+                    intCounter = CountOccurrences(inFile, strEntireFile, strSearch, intIndex, intCounter)
+                Loop
+                'Displays the number of times a word appears
+                txtDisplay.Text = "'" + strSearch.ToUpper + "'" + " appears " + intCounter.ToString + " times in the entire file."
+                'If the file doesn't exist, a message is displayed
+        End Select
+        'brings focus back to the search text box
+        txtSearch.Focus()
+    End Sub
+
+
+    'EVENT:     user presses 'Display' button
+    'Purpose:   Displays each line of text that contains the user's search word
+    '           Displays the user's search word in bold & in a different color
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        Dim strSearchWord As String = txtSearch.Text
+        Dim strFileLine As String = ""
+        Dim intIndex As Integer
+
+        'Clears any previously displayed text
+        txtDisplay.Text = ""
+
+        'prevents null reference exception
+        Call Check_If_File_Exists(inFile)
+
+        'Checks if user input is valid or if it is an empty string
+        Select Case strSearchWord
+            Case Is = String.Empty
+                txtDisplay.Text = "Invalid entry"
+
+                'finds the lines of text that the search word appears in
+            Case Else
+                Call findLinesWithSearchWord(inFile, strFileLine, intIndex, strSearchWord)
+                Call boldAndHighlight(strSearchWord)
+        End Select
+
+        'brings focus back to the search text box
+        txtSearch.Focus()
+    End Sub
+
+
+
+    'EVENT:     user clicks the 'Exit' button
+    'Purpose:   Closes the program
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+
+
+    'EVENT:     user clicks the 'Clear' button
+    'Purpose: Clears user input and output
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtDisplay.Text = ""
+        txtSearch.Text = ""
+        txtSearch.Focus()
+    End Sub
+
+
+
+    'EVENT:     User presses any key
+    'Purpose:   limits user input to only letters
+    Private Sub txtSearch_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtSearch.KeyPress
+        If (e.KeyChar < "a" OrElse e.KeyChar > "z") AndAlso
+            (e.KeyChar < "A" OrElse e.KeyChar > "Z") AndAlso
+            e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+        'Clears the output if the user types in new input
+        txtDisplay.Text = ""
+    End Sub
+
+
+End Class
